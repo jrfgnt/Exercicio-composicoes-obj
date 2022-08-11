4b4dcf5150aba6a8096b4f3d48ac0e7f6278b886
Junior Fraga <juniorfg.fg@gmail.com>
2022-08-10 23:45:05 -0300

    Exercicio de curso C#

---
+++ Resolution/Entities/Order.cs
@@ -0,0 +1,73 @@
+﻿using Resolution.Entities.Enums;
+using System.Text;
+using System.Globalization; 
+
+namespace Resolution.Entities
+{
+    internal class Order
+    {
+        CultureInfo CI = CultureInfo.InvariantCulture; 
+
+
+
+        public DateTime Moment { get; set; }
+
+        public  OrderStatus Status { get; set; }
+
+        public Client Client { get; set; }
+
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Order()
+        {
+        }
+
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem (OrderItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItem (OrderItem item)
+        {
+            Items.Remove(item); 
+        }
+
+        public double Total()
+        {
+            double sum = 0.0; 
+            foreach(OrderItem item in Items)
+            {
+                sum += item.SubTotal();
+            }
+
+            return sum; 
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Order moment:" + Moment.ToString("dd/MM/yyyy HH:MM:ss"));
+            sb.AppendLine("Order status:" + Status);
+            sb.AppendLine("Client:" + Client);
+            sb.AppendLine("Order items:"); 
+            foreach(OrderItem order in Items)
+            {
+                sb.AppendLine(order.ToString());
+            }
+            sb.AppendLine("Total price: $" + Total().ToString("F2", CI));
+
+            return sb.ToString(); 
+
+
+        }
+
+
+    }
+}
---
+++ Resolution/Program.cs
@@ -0,0 +1,62 @@
+﻿using Resolution.Entities;
+using Resolution.Entities.Enums;
+using System.Globalization; 
+
+namespace Resolution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture; 
+
+
+
+            Console.WriteLine("Enter cliente data: ");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email:");
+            string mail = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY):");
+            DateTime date = DateTime.Parse(Console.ReadLine());
+
+            Client client = new Client(name, mail, date);
+         
+
+            Console.WriteLine("Enter order data:");
+            Console.Write("Status:");
+            string status = Console.ReadLine();
+            Console.WriteLine("How many items to this order? ");
+            int nItems = int.Parse(Console.ReadLine());
+
+            OrderStatus os = Enum.Parse<OrderStatus>(status);
+
+            Order order = new Order(DateTime.Now, os, client);
+
+            for (int i =1; i<=nItems; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data:");
+                Console.Write("Product name:");
+                string productName = Console.ReadLine();
+                Console.Write("Product price:");
+                double productPrice = double.Parse(Console.ReadLine(), CI);
+                Console.Write("Quantity:");
+                int quantity = int.Parse(Console.ReadLine());
+
+                Product prod = new Product(productName, productPrice);
+
+                OrderItem orderItem = new OrderItem(quantity, productPrice, prod);
+
+                order.AddItem(orderItem);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("ORDER SUMMARY:");
+            Console.WriteLine(order);
+
+
+
+
+        }
+    }
+}
